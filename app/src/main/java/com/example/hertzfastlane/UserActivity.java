278fd4c36afc0e87fe974851dca89d2e25b2fe8e
rtrevino821 <rtrevino821@gmail.com>
2016-10-10 21:10:59 -0400

    Rudy and Derek merged code.

--- app/src/main/java/com/example/hertzfastlane/UserActivity.java
+++ app/src/main/java/com/example/hertzfastlane/UserActivity.java
@@ -1,63 +1,49 @@
 package com.example.hertzfastlane;
 
-import android.content.DialogInterface;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.view.ContextThemeWrapper;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.content.Context;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class UserActivity extends AppCompatActivity {
     //public Button button;
     final Context context = this;
+    Member member;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         final EditText etName = (EditText) findViewById(R.id.etName);
+        final TextView tvWelcome = (TextView) findViewById(R.id.tv_welcome);
         final Button bMyReservation = (Button) findViewById(R.id.bMyReservation);
-        final Button bScanner = (Button) findViewById(R.id.bScanner);
-        final Button bMap = (Button) findViewById(R.id.bMap);
-        final Button bHelp = (Button) findViewById(R.id.bHelp);
 
-        //final String
-        //Intent intent = getIntent();
-        //String name = intent.getStringExtra("name");
-        //etName.setText(name);
+        member = LoginActivity.getMember();
 
-        new AlertDialog.Builder(context).setCancelable(false)
-                .setTitle("Airport Location")
-                .setMessage("Choose Your location")
-                .setPositiveButton("Miami", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        bMap.setOnClickListener(new View.OnClickListener() {
-                                  @Override
-                                  public void onClick(View v) {
+        String name = member.getFirst_NM();
+        tvWelcome.setText("Welcome " + name + ",\nLet us know how we can help.");
 
-                                       Intent mapIntent = new Intent(UserActivity.this, MapActivity.class);
-                                      UserActivity.this.startActivity(mapIntent);
-                                 }
-                        });
-                    }
-                })
-                .setNegativeButton("Tampa", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        bMap.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-
-                                Intent mapIntent = new Intent(UserActivity.this, MapActivity2.class);
-                                UserActivity.this.startActivity(mapIntent);
-                            }
-                        });
-                    }
-                })
-                .setIcon(android.R.drawable.alert_dark_frame).setInverseBackgroundForced(true)
-                .show();
 
 
 
@@ -66,14 +52,6 @@ public class UserActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent reservationIntent = new Intent(UserActivity.this, MyReservationActivity.class);
                 UserActivity.this.startActivity(reservationIntent);
-            }
-        });
-
-        bScanner.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent registerIntent = new Intent(UserActivity.this, QrScanner.class);
-                UserActivity.this.startActivity(registerIntent);
             }
         });
 
@@ -86,12 +64,64 @@ public class UserActivity extends AppCompatActivity {
             }
         });
 */
-        bHelp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent helpIntent = new Intent(UserActivity.this, HelpActivity.class);
-                UserActivity.this.startActivity(helpIntent);
-            }
-        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("User Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_maps:
+                startActivity(new Intent(this, MapActivity.class));
+                return true;
+            case R.id.action_help:
+                startActivity(new Intent(this, HelpActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
